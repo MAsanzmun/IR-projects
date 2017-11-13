37a7a5fffca5970c70cd8490ac33d8ddababa22f
MAsanzmun <miguelsanzmun@gmail.com>
2017-11-13 19:04:18 +0100

    Added styles for the cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListProjectsComponent } from './compts/list-projects/list-projects.component';
+
+import { ProjectsService } from './service/projects.service';
+import { APP_ROUTING } from './app.routing';
+import { Project } from './interfaces/project';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProjectsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [ProjectsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
